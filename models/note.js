f975e02baa33fcd07b47637bb2adcea747386a31
jabss <jabrijuhinin93@gmail.com>
2025-03-16 23:56:18 +0800

    add date field in note model

--- models/note.js
+++ models/note.js
@@ -10,6 +10,10 @@ const noteSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
+  },
+  date: {
+    type: Date,  // Store the date as a Date type
+    required: true
   }
 })
 
