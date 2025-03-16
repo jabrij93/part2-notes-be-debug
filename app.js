1e171d986c3cc4dabb7000358cb3629f3f032424
jabss <jabrijuhinin93@gmail.com>
2025-03-17 00:07:00 +0800

    add testing router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const testingRouter = require('./controllers/testing')
 
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -34,6 +35,7 @@ app.use(middleware.requestLogger)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/testing', testingRouter)
 
 // if (process.env.NODE_ENV === 'test') {
 //   logger.info('Backend is running in', process.env.NODE_ENV, 'mode')
